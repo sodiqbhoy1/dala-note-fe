f0342bbc49fb929d67639718d3afafdc599ece9d
sodiqbhoy1 <adamubabasodiq@gmail.com>
2025-09-08 22:32:33 +0100

    add OCRExtractor component for image-to-text extraction functionality

--- dala-note/src/App.jsx
+++ dala-note/src/App.jsx
@@ -1,10 +1,12 @@
+import OCRExtractor from './components/OCRExtractor'
 import './components/styles/app.css'
 
 function App() {
 
   return (
     <>
-     <h1 className='bg-amber-600'>Hello world</h1>
+     
+     <OCRExtractor/>
     </>
   )
 }
